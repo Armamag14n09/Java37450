1c359a032d29c6d84fad1a81142f307d93463b81
Armando Maldonado <maga_2814@hotmail.com>
2022-06-13 23:54:02 -0500

    Entrga Array Armando Maldon

--- app.js
+++ app.js
@@ -1,63 +1,29 @@
-//const numeroIngresado = Number(prompt('Ingrese un numero'))
-//let numero1=0;
-//let numero2=1
-//let numero3=0
 
-//for(let contador=0; contador<40; contador++){
-   
-  //  if(contador% 2===0){
-        //console.log(contador +"Numero!!")
-    //    continue
-    //}
-    //console.log(contador)
-//} 
+class Producto{
+    constructor(id, nombre, precio){
+    this.id = id
+    this.nombre = nombre
+    this.precio = precio
 
-//console.log("Fin del programa")
-
-function holaMundo(){
-            alert("Hola Mundo!")
+    }
 }
 
-function usuario(){ 
-            let usuario= prompt("Intodusca el password del Admin")
-            while (true){
-                if (usuario === 'Armando'){ 
-                break
-            }
-                alert("Password Incorrecto!")
-                usuario=prompt('Intodusca password del Admin')
-                 }
-                 //alert("Bienvenido "+ usuario +" a la clse de JS")
-                 //holaMundo()
+const productos=[
+    new Producto(1,'Memoria',550),
+    new Producto(2,'Teclado',250),
+    new Producto(3,'Mouse',153),
+]
+
+function agregarProducto(){
+    let id = Number( prompt('Ingrese id del producto') )
+    let nombre = prompt('Ingrese nombre del producto')
+    let precio = Number( prompt('Ingrese precio del producto') )
 }
-usuario()
 
-function infoUsuario(){
+productos.push( new Producto (id, nombre, precio) )
+//productos.push( { id:id, nombre:nombre, precio:precio })
 
-        let nombre= prompt('Ingrese su Nombre')
-        let edad= prompt('Ingrese su Edad:')
-        let curso= prompt('Ingrese su Curso')
+console.log(productos)  
 
-
-        console.log("Nombre: " + nombre)
-        console.log("Edad: " + edad + "años")
-        console.log("Cursando: " + curso)
-
-            evaluarEdad(edad)
-}
-infoUsuario()
-
-function evaluarEdad(edad){
-               //console.log("Bienvenido " + comprador)
-            if (edad < 18){
-                alert("Usted no puede comprar articulos Gamer!")
-            }else {
-                alert("Bienvenido, que desea comprar?")
-            }
-}
-                  
-        
-    //let edad = Number (prompt("Ingrese nombre de Usuario"))
-        
-// evaluarEdad(edad)
-
+agregarProducto()
+console.log(productos)
